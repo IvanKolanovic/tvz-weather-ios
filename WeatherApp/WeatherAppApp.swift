d1e25b704bd54491da2ed715f776deee2e6f5495
Ivan Kolanović <ivan.kolanovic@tenzen.hr>
2025-01-26 20:08:49 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ivan Kolanović on 26.01.2025..
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
